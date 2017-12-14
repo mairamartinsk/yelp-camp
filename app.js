47cdf5c24154da27220917abc570e27b4970b615
Maira Martins K <mairamartinsk@gmail.com>
2017-12-14 15:42:57 +0100

    Refactor new and index routes to use mongodb

--- app.js
+++ app.js
@@ -12,15 +12,17 @@ mongoose.Promise = global.Promise;
 
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 var Campground = mongoose.model('Campground', campgroundSchema);
 
 // DB Add Test
 // Campground.create({
-//   name: 'Mountain Goat\'s Rest',
-//   image: 'https://cdn.pixabay.com/photo/2017/10/28/23/18/indians-2898463_640.jpg'
+//   name: 'The Goat\'s Rest',
+//   image: 'https://cdn.pixabay.com/photo/2017/10/28/23/18/indians-2898463_640.jpg',
+//   description: 'The perfect refuge for goats. Sheep are also welcome.'
 // }, function(error, camp){
 //   if (error) {
 //     console.log('There was an error.');
@@ -34,17 +36,24 @@ app.get('/', function(req, res) {
   res.render('landing');
 });
 
+// INDEX ROUTE (show all campgrounds)
 app.get('/campgrounds', function(req, res) {
   // Retrieve all campgrounds from DB
   Campground.find({}, function(error, camps){
     if (error) {
       console.log('There was an error.');
     } else {
-      res.render('campground', {campgrounds: camps});
+      res.render('index', {campgrounds: camps});
     }
   });
 });
 
+// NEW ROUTE (show form to create new camp)
+app.get('/campgrounds/new', function(req, res) {
+  res.render('new');
+});
+
+// CREATE ROUTE (add new camp to DB)
 app.post('/campgrounds', function(req, res) {
   // get data from form and add to campgrounds array
   var name = req.body.name;
@@ -61,8 +70,17 @@ app.post('/campgrounds', function(req, res) {
   });
 });
 
-app.get('/campgrounds/new', function(req, res) {
-  res.render('new.ejs');
+// SHOW ROUTE (get more info about one campsite)
+app.get('/campgrounds/:id', function(req, res){
+  // find the campground with provided ID
+  Campground.findById(req.params.id, function(error, retrieveCamp){
+    if (error) {
+      console.log(error);
+    } else {
+      // render show template for that campground
+      res.render('show', {campground: retrieveCamp});
+    }
+  });
 });
 
 app.listen(3000, function() {
