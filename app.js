222e9983745e9b4557feed4f1c0817e11f96f55b
Maira Martins K <mairamartinsk@gmail.com>
2018-01-23 15:55:51 +0100

    Update server port for Heroku deployment

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ app.use(indexRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 app.use('/campgrounds', campgroundRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port 3000');
 });
